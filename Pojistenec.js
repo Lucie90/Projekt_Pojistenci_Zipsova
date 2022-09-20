49a35093e2970c2afafdcb3dd143ddf25f39b722
Lucie90 <113973971+Lucie90@users.noreply.github.com>
2022-09-20 10:46:14 +0200

    Add files via upload

---
+++ Pojistenec.js
@@ -0,0 +1,8 @@
+let lucka = new Pojistenec("Alena", "Nová", "123123123", "20");
+let jirka = new Pojistenec("Jirka", "Zelený", "654654654", "60")
+let michal = new Pojistenec("Michal", "Druhý")
+
+console.log(lucka)
+console.log(jirka)
+console.log(michal)
+
---
+++ index.js
@@ -0,0 +1,36 @@
+    class Pojistenec{
+        constructor(jmeno, prijmeni, telefon, vek){
+            this.prijmeni = prijmeni;
+            this.jmeno = jmeno;
+            this.telefon = telefon;
+            this.vek = vek;
+        }
+    }
+
+    const jmeno = document.querySelector(".jmeno ")
+    const prijmeni = document.querySelector(".prijmeni ")
+    const telefon = document.querySelector(".telefon ")
+    const vek = document.querySelector(".vek ")
+    const tlacitko = document.querySelector(".button")
+    const seznam = document.querySelector(".tabulka-pojistenych")
+
+    const pojistenci = []
+
+
+
+    tlacitko.addEventListener("click",function (e) {
+        e.preventDefault();
+
+        const pojistenec = new Pojistenec(jmeno.value, prijmeni.value, telefon.value, vek.value);
+
+        pojistenci.push(pojistenec);
+        const polozka = document.createElement("tr");
+    
+        polozka.innerHTML = `<td> ${pojistenec.jmeno + " " + pojistenec.prijmeni} </td>    
+            <td> ${ pojistenec.telefon} </td> <td> ${pojistenec.vek}</td>`
+        seznam.appendChild(polozka)
+    
+
+        console.log(pojistenci);
+});
+
